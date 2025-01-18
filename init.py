5b2620335d67642d5040472d734f15a70fe1b52e
emustard1 <edward-mustard@hotmail.com>
2025-01-18 22:42:59 +0000

    Removed about page, finished footer and header html

--- init.py
+++ init.py
@@ -8,18 +8,15 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/about-me')
-def about_me():
-    return 'About page'
-
-
-@app.route('/contact')
-def contact():
-    return 'THis is the contact page'
 
 @app.route('/projects')
 def projects():
     return 'This is the project page'
+
+
+@app.route('/blog')
+def blog():
+    return 'This will be the blog page'
 
 if __name__ == "__main__":
     app.run(debug=True)
